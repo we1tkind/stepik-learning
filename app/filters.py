6fd54d0fd9bcfad30ce678d297188eb1126e1788
Alexander Erdyakov <sa@prg.re>
2021-02-15 13:18:20 +0300

    добавил страницу со всеми преподавателями

--- app/filters.py
+++ app/filters.py
@@ -1,1 +1,12 @@
-# -*- coding: utf-8 -*-
+import pymorphy2
+
+from db import JsonDB
+
+morph = pymorphy2.MorphAnalyzer()
+db = JsonDB('data')
+
+
+def word_agree_with_number(number: int, word: str) -> str:
+    """Возвращает форму слова согласованную с числительным."""
+    morph_word = morph.parse(word)[0]
+    return morph_word.make_agree_with_number(number).word
