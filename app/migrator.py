a737135c927eefc8407af2bf0b2fd1aa8a1d8ff7
Alexander Erdyakov <sa@prg.re>
2021-02-15 13:47:47 +0300

    изменил формат output-данных мигратора

--- app/migrator.py
+++ app/migrator.py
@@ -8,7 +8,7 @@ from data import goals, teachers
 def migrate(file_path, data):
     file_path.parent.mkdir(parents=True, exist_ok=True)
     with open(file_path, 'w', encoding='utf8') as f:
-        json.dump(data, f, indent=4, sort_keys=True, ensure_ascii=False)
+        json.dump(data, f, indent=4, sort_keys=False, ensure_ascii=False)
 
 
 def migrate_teachers(path, teachers):
@@ -19,7 +19,13 @@ def migrate_teachers(path, teachers):
 
 def migrate_goals(path, goals):
     file_path = path.joinpath('goals.json')
-    migrate(file_path, {'goals': goals})
+    emoji = iter('⛱🏫🏢🚜🖥️')
+    migrate(
+        file_path,
+        {'goals': [
+            {'id': k, 'title': v, 'emoji': next(emoji)}
+            for k, v in goals.items()
+        ]})
 
 
 def migrate_all():
